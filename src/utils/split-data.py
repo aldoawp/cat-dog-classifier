45fa80aa8d667b59c2c867f63d8be4dcb5eb4662
Aldo Arista <aristaaldo2@gmail.com>
2025-09-20 23:59:55 +0700

    init

---
+++ src/utils/split-data.py
@@ -0,0 +1,48 @@
+import os
+import shutil
+import random
+
+SOURCE_DIR = "src/data/raw"
+OUTPUT_DIR = "src/data"
+
+train_ratio = 0.7
+validation_ratio = 0.2
+test_ratio = 0.1
+
+split_folder = ["train", "validation", "test"]
+class_folder = ["cats", "dogs"]
+
+# Check whether the folder exist or not
+for split in split_folder:
+  for cls in class_folder:
+    os.makedirs(os.path.join(OUTPUT_DIR, split, cls), exist_ok=True)
+
+def copy_files(file_list, dir, class_name):
+    for file in file_list:
+      src = os.path.join(SOURCE_DIR, dir)
+      dst = os.path.join(OUTPUT_DIR, dir, class_name, file)
+      shutil.copy2(src, dst)
+
+def split_data(class_name):
+    src_dir = os.path.join(SOURCE_DIR, class_name)
+    files = os.listdir(src_dir)
+    random.shuffle(files)
+
+    n_files = len(files)
+    n_train_files = int(train_ratio * n_files)
+    n_validation_files = int(validation_ratio * n_files) 
+
+    train_files = files[:n_train_files]
+    validation_files = files[n_train_files:n_train_files + n_validation_files]
+    test_files = files[n_train_files + n_validation_files:]
+
+    print("Start splitting and copying files...")
+
+    copy_files(train_files, "train", class_name)
+    copy_files(train_files, "validation", class_name)
+    copy_files(train_files, "test", class_name)
+
+    print("Files split and copy successful!")
+
+split_data("cats")
+split_data("dogs")
